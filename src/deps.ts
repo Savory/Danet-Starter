a369bf522e4e59ade004653d412b4d1fce93adf9
Thomas Cruveilher <38007824+Sorikairox@users.noreply.github.com>
2022-08-29 04:28:32 +0200

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export { Module, Controller, Injectable, Get, Post, Put, Delete, Param, Body, DanetApplication } from 'https://deno.land/x/danet@v0.6.2/mod.ts';
+export { Module, Controller, Injectable, Get, Post, Put, Delete, Param, Body, DanetApplication } from 'https://deno.land/x/danet@v0.7.1/mod.ts';
