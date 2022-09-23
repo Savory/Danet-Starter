ccb47b51af200ecb132ed325829a05c4f2070286
maplecfiv <maplecfiv@gmail.com>
2022-09-23 19:14:49 +0800

    bump version

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export { Module, Controller, Injectable, Get, Post, Put, Delete, Param, Body, DanetApplication } from 'https://deno.land/x/danet@v0.7.1/mod.ts';
+export { Module, Controller, Injectable, Get, Post, Put, Delete, Param, Body, DanetApplication } from 'https://deno.land/x/danet@v0.7.2/mod.ts';
