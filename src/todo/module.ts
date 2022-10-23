f8766a398aac1b4fb7b2c1eaeabbd32d8b0e07ca
Sorikairo <sorikairox@gmail.com>
2022-10-23 19:12:03 +0900

    refactor: put back in memory as default

--- src/todo/module.ts
+++ src/todo/module.ts
@@ -2,15 +2,15 @@ import { TodoController } from "./controller.ts";
 import { TodoService } from "./service.ts";
 import { Module, TokenInjector } from "../deps.ts";
 import { USER_REPOSITORY } from "./constant.ts";
-import { DatabaseModule } from "../database/module.ts";
-import { PostgresRepository } from "./postgres-repository.ts";
+// import { DatabaseModule } from "../database/module.ts";
+import { InMemoryTodoRepository } from './in-memory-repository.ts';
 
 @Module({
-  imports: [DatabaseModule],
+  // imports: [DatabaseModule],
   controllers: [TodoController],
   injectables: [
-    new TokenInjector(PostgresRepository, USER_REPOSITORY),
+    new TokenInjector(InMemoryTodoRepository, USER_REPOSITORY),
     TodoService,
-  ], // change mongoDbRepository by any custom repository using other database engine if needed
+  ], // change InMemoryTodoRepository by any custom repository using other database engine if needed
 })
 export class TodoModule {}
