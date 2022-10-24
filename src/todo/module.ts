fe0eaf550aabe1084aa4552709e100456cf74468
Sorikairo <sorikairox@gmail.com>
2022-10-24 18:13:02 +0900

    refactor: put back in memory as default

--- src/todo/module.ts
+++ src/todo/module.ts
@@ -2,15 +2,11 @@ import { TodoController } from './controller.ts';
 import { TodoService } from './service.ts';
 import { Module, TokenInjector } from 'danet/mod.ts';
 import { USER_REPOSITORY } from './constant.ts';
-import { DatabaseModule } from '../database/module.ts';
-import { PostgresService } from '../database/postgres.service.ts';
-import { PostgresRepository } from './postgres-repository.ts';
-
+import { InMemoryTodoRepository } from './in-memory-repository.ts';
 @Module({
-  imports: [DatabaseModule],
   controllers: [TodoController],
   injectables: [
-    new TokenInjector(PostgresRepository, USER_REPOSITORY),
+    new TokenInjector(InMemoryTodoRepository, USER_REPOSITORY),
     TodoService,
   ], // change InMemoryTodoRepository by any custom repository using other database engine if needed
 })
