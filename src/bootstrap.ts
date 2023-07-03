436235971a1cc4deea21eff2f4fc6b14a15273dc
Sorikairo <sorikairox@gmail.com>
2023-07-03 23:17:22 +0900

    chore: upgrade danet and use official dotenv

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,9 @@
+import 'dotenv/load.ts';
 import { AppModule } from './app.module.ts';
 import { DanetApplication } from 'danet/mod.ts';
-import { configAsync } from 'dotenv/mod.ts';
 import { loggerMiddleware } from './logger.middleware.ts';
 import { SpecBuilder, SwaggerModule } from 'danet_swagger/mod.ts';
 export const bootstrap = async () => {
-  await configAsync({ export: true });
   const application = new DanetApplication();
   await application.init(AppModule);
   const spec = new SpecBuilder()
